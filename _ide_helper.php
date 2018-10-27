3026d33c52b2d63c63c7bb43e251723e479ab80d
ZicLemon <534076424@qq.com>
2018-10-27 17:34:54 +0800

    api/user

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.5.19 on 2018-10-21 16:48:07.
+ * Generated for Laravel 5.5.19 on 2018-10-27 17:17:40.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
+    'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
 ], function ($api) {
 
     $api->group([
@@ -56,6 +57,21 @@ $api->version('v1', [
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
+    });
+
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
+    ],function($api){
+        // 游客可以访问的接口
+
+
+        //需要token验证的接口
+        $api->group(['middleware' => 'api.auth'],function($api){
+            $api->get('user','UsersController@me')
+                ->name('api.user.show');
+        });
     });
 
 });
