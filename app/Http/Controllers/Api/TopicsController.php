b499c763113ef2fbd19ab4ca453282de0af69a0f
ZicLemon <534076424@qq.com>
2018-11-04 20:05:50 +0800

    topic delete api

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -9,21 +9,29 @@ use Illuminate\Http\Request;
 
 class TopicsController extends Controller
 {
-    public function store(TopicRequest $request,Topic $topic)
+    public function store(TopicRequest $request, Topic $topic)
     {
         $topic->fill($request->all());
         $topic->user_id = $this->user()->id;
         $topic->save();
 
-        return $this->response->item($topic,new TopicTransformer())
+        return $this->response->item($topic, new TopicTransformer())
             ->setStatusCode(201);
     }
 
-    public function update(TopicRequest $request,Topic $topic)
+    public function update(TopicRequest $request, Topic $topic)
     {
-        $this->authorize('update',$topic);
+        $this->authorize('update', $topic);
 
         $topic->update($request->all());
-        return $this->response->item($topic,new TopicTransformer());
+        return $this->response->item($topic, new TopicTransformer());
+    }
+
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy',$topic);
+
+        $topic->delete();
+        return $this->response->noContent();
     }
 }
